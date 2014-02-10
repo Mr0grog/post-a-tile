7a4101f3956df8f2ebc24a3d05cc131a4dffb270
Rob Brackett <rob@robbrackett.com>
2014-02-09 21:09:39 -0800

    First commit

---
+++ server.js
@@ -0,0 +1,58 @@
+var PORT = process.env.PORT || process.argv[2] || 3000;
+var DEBUG = true;
+
+// dependencies
+var express = require("express");
+var nodetiles = require("nodetiles-core");
+var projector = nodetiles.projector;
+var fs = require("fs");
+
+// web serving
+var app = module.exports = express();
+app.use("/assets", express.static(__dirname + "/assets"));
+app.use(express.bodyParser());
+
+app.configure("development", function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure("production", function(){
+  app.use(express.errorHandler());
+});
+
+
+// Serve index.html
+app.get("/", function(req, res) {
+  res.sendfile(__dirname + "/index.html");
+});
+
+// Post some GeoJSON data to a tile coordinate, get a rendered tile back!
+app.post('/tiles/:zoom/:x/:y.png', function(req, res) {
+  var zoom = parseInt(req.params.zoom, 10),
+      x = parseInt(req.params.x, 10),
+      y = parseInt(req.params.y, 10),
+      bounds = projector.util.tileToMeters(x, y, zoom);
+  var data = JSON.parse(req.body.data);
+  
+  var map = new nodetiles.Map();
+  map.addStyle(fs.readFileSync("./style.mss", "utf8"));
+  map.addData({
+    sourceName: "data", // for the stylesheet to use
+    getShapes: function(minX, minY, maxX, maxY, mapProjection) {
+      return projector.project.FeatureCollection("EPSG:4326", mapProjection, data);
+    }
+  });
+  
+  map.render({
+    bounds: {minX: bounds[0], minY: bounds[1], maxX: bounds[2], maxY: bounds[3]},
+    width: 256,
+    height: 256,
+    zoom: zoom,
+    callback: function(err, canvas) {
+      canvas.createPNGStream().pipe(res);
+    }
+  });
+});
+
+app.listen(PORT);
+console.log("Express server listening on port %d in %s mode", PORT, app.settings.env);
---
+++ style.mss
@@ -0,0 +1,7 @@
+#data {
+	marker-fill: #ff530d;
+	marker-line-color: #fff;
+	marker-width: 8;
+	marker-line-width: 2;
+	marker-allow-overlap: true;
+}
